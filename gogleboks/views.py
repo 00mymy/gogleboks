a23674e73a1dc6dd5158a8c77f502f2ed81751cb
dyseo <dyseo@nethru.co.kr>
2016-03-17 16:05:52 +0900

    DB에서 Review 목록 불러와 출력하도록 템플릿 수정

--- gogleboks/views.py
+++ gogleboks/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Review
 
 # Create your views here.
 def review_list(request):
-    return render(request, 'gogleboks/review_list.html', {})
+    reviews = Review.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'gogleboks/review_list.html', {'reviews':reviews})
