499ef2b4f1699ad32a3487f4e25740b5cf652167
dyseo <dyseo@nethru.co.kr>
2016-03-18 16:42:35 +0900

    Review 작성 및 편집 기능 추가

--- gogleboks/views.py
+++ gogleboks/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
 def review_list(request):
@@ -10,3 +13,31 @@ def review_list(request):
 def review_detail(request, pk):
     review = get_object_or_404(Review, pk=pk)
     return render(request, 'gogleboks/review_detail.html', {'review':review})
+
+def review_new(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.author = request.user
+            review.created_date = timezone.now()
+            review.published_date = timezone.now()
+            review.save()
+            return redirect('gogleboks.views.review_detail',pk=review.pk)
+    else:
+        form = ReviewForm()
+    return render(request, 'gogleboks/review_edit.html', {'form':form})
+
+def review_edit(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.author = request.user
+            review.published_date = timezone.now()
+            review.save()
+            return redirect('gogleboks.views.review_detail', pk=review.pk)
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'gogleboks/review_edit.html', {'form': form})
